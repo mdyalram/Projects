6bfef967cba3021bdb65d8eb73e9a1605be317e6
Michael Dyalram <130186671+mdyalram@users.noreply.github.com>
2023-04-28 14:28:55 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+PI = 42
+//Wont let you
+
+//var is reassignable and redeclarable and let is only reassignable
+
+//var can be reassined and redeclared, you cannot do either using const
+
+//let is reassignable but not const, nor is const redeclarable
+
+//it is when variables are put at the top of the scope they're declared in
